5b6d6a74c52224fb54e33ad3e234d87bf463a635
Esther Alter <subalterngames@gmail.com>
2024-07-26 16:23:02 -0400

    Fixed indentations

--- generate_readme.py
+++ generate_readme.py
@@ -2,4 +2,4 @@ import re
 from pathlib import Path
 
 text = Path("src/lib.rs").read_text(encoding="utf-8")
-Path("README.md").write_text("# softbuffer-rgb\n\n" + "\n".join([line.strip() for line in re.findall(r"^//!(.*)$", text, flags=re.MULTILINE)]))
+Path("README.md").write_text("# softbuffer-rgb\n\n" + "\n".join(re.findall(r"^//!(.*)$", text, flags=re.MULTILINE)))
